71478958cd184de0744c1b725c55ab1ebe2fac50
thor.bagge <thor.bagge@bdc61cda-7c69-11de-a32a-85815177f542>
2009-07-30 10:20:30 +0000

    Problem 1 solved!

--- trunk/ProjectEuler/src/projecteuler/problems/thor/ProblemThor1.java
+++ trunk/ProjectEuler/src/projecteuler/problems/thor/ProblemThor1.java
@@ -12,11 +12,20 @@ package projecteuler.problems.thor;
 public class ProblemThor1 extends ProblemThor {
 
     public void init() {
-
+        result = 0;
     }
 
+    /*
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+       we get 3, 5, 6 and 9.The sum of these multiples is 23.
+       Find the sum of all the multiples of 3 or 5 below 1000.
+    */
     public void run() {
-        result = 5;
+        for(int i = 1; i < 1000; i++){
+            if(i%3 == 0 || i%5 == 0){
+                result += i;
+            }
+        }
     }
 
     public int problemNumber() {
