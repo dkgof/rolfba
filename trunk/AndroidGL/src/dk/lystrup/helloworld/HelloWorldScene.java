d9a5ebe913bf6bc66524ff8cb9710dc38956228d
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2011-01-15 09:24:37 +0000

    Added TextureQuad node, Fixed scene init() never being called, Changed around in the fields of AbstractNode to make some protected, and added final keyword to others

--- trunk/AndroidGL/src/dk/lystrup/helloworld/HelloWorldScene.java
+++ trunk/AndroidGL/src/dk/lystrup/helloworld/HelloWorldScene.java
@@ -13,6 +13,8 @@ import dk.lystrup.androidgl.math.Vector3;
 import dk.lystrup.androidgl.nodes.CameraNode;
 import dk.lystrup.androidgl.nodes.ModelData;
 import dk.lystrup.androidgl.nodes.ModelNode;
+import dk.lystrup.androidgl.nodes.Node;
+import dk.lystrup.androidgl.nodes.TextureQuad;
 import dk.lystrup.androidgl.textures.StaticTexture;
 import dk.lystrup.androidgl.textures.Texture;
 
@@ -22,33 +24,15 @@ import dk.lystrup.androidgl.textures.Texture;
  */
 public class HelloWorldScene extends AbstractScene {
 
-    private float vertices[] = {
-        -0.5f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f,};
-
-    private float textures[] = {
-        0.0f, 0.0f,
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-        1.0f, 0.0f};
-
-    private short[] indices = {0, 1, 2, 0, 2, 3};
-
     @Override
     protected void customInit() {
-        ModelData data = new ModelData();
-        data.setIndices(indices);
-        data.setVertices(vertices);
-        data.setTexturecoords(textures);
 
         CameraNode camera = new CameraNode();
         camera.setPosition(0,0,5);
         root.attachNode(camera);
 
-        ModelNode model = new ModelNode(data);
-
+        Node model = new TextureQuad();
+        
         Texture floraTex = new StaticTexture(R.drawable.flora);
 
         Actuator act = new ConstantRotationActuator(new AxisAngle(90, Vector3.UnitX));
@@ -56,7 +40,7 @@ public class HelloWorldScene extends AbstractScene {
         model.setTexture(floraTex);
         model.setPosition(-1, 0, 0);
 
-        ModelNode model2 = new ModelNode(data);
+        Node model2 = new TextureQuad();
         model2.setTexture(floraTex);
         model2.setPosition(1,0,0);
         Actuator act2 = new ConstantRotationActuator(new AxisAngle(45, Vector3.UnitY));
