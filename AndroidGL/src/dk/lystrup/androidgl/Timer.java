3b08ee9567689b921016639fa3815de85e1c920b
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2011-04-20 23:02:07 +0000

    Update to javadoc

--- AndroidGL/src/dk/lystrup/androidgl/Timer.java
+++ AndroidGL/src/dk/lystrup/androidgl/Timer.java
@@ -2,7 +2,7 @@
 package dk.lystrup.androidgl;
 
 /**
- * Timer models a nano precision timer for messuring time between frames and other
+ * Timer models a nanosecond precision timer for measuring time between frames and other
  * timing that requires precise delta values.
  * @author Rolf
  */
@@ -16,6 +16,9 @@ public class Timer {
         lastDelta = 0;
     }
 
+    /**
+     * Start this timer
+     */
     public void start() {
         if(!started) {
             start = System.nanoTime();
@@ -23,6 +26,9 @@ public class Timer {
         }
     }
 
+    /**
+     * Reset this timer
+     */
     public void reset() {
         if(started) {
             start = System.nanoTime();
@@ -30,6 +36,10 @@ public class Timer {
         }
     }
 
+    /**
+     * Return the time in seconds since this timer was started
+     * @return seconds since timer was started
+     */
     public float getTime() {
         if(started) {
             return nanoToSeconds(System.nanoTime() - start);
@@ -38,6 +48,10 @@ public class Timer {
         throw new IllegalStateException("Timer must be started before use");
     }
 
+    /**
+     * Get the time in seconds since last call to getDelta
+     * @return seconds since last getDelta call
+     */
     public float getDelta() {
         long now = System.nanoTime();
         float delta = nanoToSeconds(now-lastDelta);
@@ -45,6 +59,11 @@ public class Timer {
         return delta;
     }
 
+    /**
+     * Convert nanoseconds to seconds
+     * @param nanoTime the time in nanoseconds
+     * @return the time in seconds
+     */
     private float nanoToSeconds(long nanoTime) {
         return (float) (nanoTime / 1000000000.0);
     }
