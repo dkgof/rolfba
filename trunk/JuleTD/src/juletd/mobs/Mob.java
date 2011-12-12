a255b0e9087c86fe7a00399f5baace50632d57dd
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2011-12-12 17:12:49 +0000

    Some more javadoc

--- trunk/JuleTD/src/juletd/mobs/Mob.java
+++ trunk/JuleTD/src/juletd/mobs/Mob.java
@@ -5,10 +5,19 @@
 package juletd.mobs;
 
 /**
- *
+ * Models a mob (Enemy) in the World
  * @author Rolf
  */
 public interface Mob {
+    /**
+     * Retrieve the current health of this mob
+     * @return the current health of this mob
+     */
     public float getHealth();
+    
+    /**
+     * Set the current health of this mob
+     * @param health the health to set
+     */
     public void setHealth(float health);
 }
--- trunk/JuleTD/src/juletd/towers/projectiles/Projectile.java
+++ trunk/JuleTD/src/juletd/towers/projectiles/Projectile.java
@@ -5,9 +5,13 @@
 package juletd.towers.projectiles;
 
 /**
- *
+ * Models a projectile in the World, projectiles can collide with Mobs
  * @author Rolf
  */
 public interface Projectile {
+    /**
+     * Retrieve the damage this projectile does
+     * @return the damage of this projectile
+     */
     public float getDamage();
 }
