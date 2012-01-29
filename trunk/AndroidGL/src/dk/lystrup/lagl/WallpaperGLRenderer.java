9e9a652b8918f48c74c93a4760641c2291d94451
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2012-01-29 10:34:08 +0000

    Use GLWallpaperService instead, as this already has much of the work done.
    Update to use GLES20
    
    Add WallpaperGLRenderer around LAGLRenderer

---
+++ trunk/AndroidGL/src/dk/lystrup/lagl/WallpaperGLRenderer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.lystrup.lagl;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ *
+ * @author Rolf
+ */
+public class WallpaperGLRenderer implements GLWallpaperService.Renderer {
+
+    private LAGLRenderer laglRenderer;
+    
+    public WallpaperGLRenderer(Scene s) {
+        laglRenderer = new LAGLRenderer(s);
+    }
+    
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        laglRenderer.surfaceCreated();
+    }
+
+    public void onSurfaceChanged(GL10 unused, int w, int h) {
+        laglRenderer.surfaceChanged(w, h);
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        laglRenderer.drawFrame();
+    }
+    
+}
