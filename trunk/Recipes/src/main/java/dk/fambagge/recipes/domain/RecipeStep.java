514999a90ab8f7742137c62b65cc351ad7c5768f
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2014-03-09 17:23:14 +0000

    Add RecipeStep Hibernation data, Setup OneToMany binding for RecipeSteps in Recipe

--- trunk/Recipes/src/main/java/dk/fambagge/recipes/domain/RecipeStep.java
+++ trunk/Recipes/src/main/java/dk/fambagge/recipes/domain/RecipeStep.java
@@ -6,21 +6,57 @@
 
 package dk.fambagge.recipes.domain;
 
+import java.io.Serializable;
+import javax.persistence.*;
+
 /**
  *
  * @author Gof
  */
-public class RecipeStep {
+@Entity
+@Table( name = "RecipeSteps" )
+public class RecipeStep implements Serializable {
+    private int id;
     private String description;
 
     public RecipeStep() {
-        
+        id = -1;
+        this.description = "";
+    }
+    
+    public RecipeStep(String description) {
+        id = -1;
+        this.description = description;
     }
     
     /**
      * @return the description
      */
+    @Column( name = "description", nullable = false)
     public String getDescription() {
         return description;
+    }
+
+    /**
+     * @return the id
+     */
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
