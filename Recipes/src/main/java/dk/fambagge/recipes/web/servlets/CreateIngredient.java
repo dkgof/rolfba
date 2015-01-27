369f2e9e8e341b8a8439f78b2e078ec171455f8c
rolfba <rolfba@bdc61cda-7c69-11de-a32a-85815177f542>
2015-01-27 17:30:13 +0000

    Converted action jsp's to servlets Added PrimeFaces framework

---
+++ Recipes/src/main/java/dk/fambagge/recipes/web/servlets/CreateIngredient.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.fambagge.recipes.web.servlets;
+
+import dk.fambagge.recipes.db.MeasureType;
+import dk.fambagge.recipes.domain.Ingredient;
+import dk.fambagge.recipes.domain.Measure;
+import dk.fambagge.recipes.web.beans.CreateIngredientData;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ *
+ * @author Gof
+ */
+public class CreateIngredient extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            CreateIngredientData data = new CreateIngredientData();
+            try {
+                BeanUtils.populate(data, request.getParameterMap());
+            } catch (IllegalAccessException|InvocationTargetException ex) {
+                Logger.getLogger("Recepies").log(Level.SEVERE, "Error populating bean with data", ex);
+            }
+            
+            Measure preferredMeasure = MeasureType.getTypeFromString(data.getPreferedMeasure());
+
+            Ingredient ingredient = new Ingredient();
+            ingredient.setName(data.getName());
+            ingredient.setEnergyPerHundred(data.getEnergyPerHundred());
+            ingredient.setWeightToVolume(data.getWeightToVolume());
+            ingredient.setPreferredMeasure(preferredMeasure);
+            ingredient.save();
+            
+            out.write("true");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Create Ingredient in Database";
+    }// </editor-fold>
+
+}
